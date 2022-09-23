38ffc8fb748135a175793e52cf9ed922f37b20ac
HIANDIC <leaands25@gmail.com>
2022-09-23 16:57:02 +0200

    script is modified

--- caseFunction.sh
+++ caseFunction.sh
@@ -2,7 +2,7 @@
 
 # taking inputs from user
 # here function is being used to take inputs from user
-input() {
+inputs() {
     read -p "Enter first integer : " int1
     read -p "Enter second integer : " int2
 }
@@ -11,7 +11,7 @@ input() {
 # for this here a function is declared
 exitPrompt() {
     read -p "Do you want to continue? [y]es or [n]o :  " answer
-    if (( $answer == 'n' )); then
+    if [[ $answer == 'n' ]]; then
         echo "Exiting the script. Have a nice day..."
         sleep 3
         exit 
@@ -27,7 +27,7 @@ while(true)
     clear
     # here given string will be displayed on the screen
     printf "Choose from the following operations : \n"
-    printf "[a]Addition\n[b]Subtraction\n[c]Multiplication\[d]Division\n"
+    printf "[a]Addition\n[b]Subtraction\n[c]Multiplication\n[d]Division\n"
     printf "\n"
     printf "\n"
     printf "#####################################\n"
