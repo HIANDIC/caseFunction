e39f1b69bf6dd1078a0b56a64459b9202e5eec7f
HIANDIC <leaands25@gmail.com>
2022-09-23 16:06:27 +0200

    declared input function

--- caseFunction.sh
+++ caseFunction.sh
@@ -1,6 +1,11 @@
 #! /bin/bash
 
 # taking inputs from user
+# here function is being used to take inputs from user
+input() {
+    read -p "Enter first integer : " int1
+    read -p "Enter second integer : " int2
+}
 
 # functions for question if going on or quit
 
